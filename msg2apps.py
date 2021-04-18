8f3c7f2a4a2de19e432cc0dd5ed3406d656ae07f
daksim <36877741+daksim@users.noreply.github.com>
2021-04-18 11:15:21 +0800

    Support Telegram now.

--- msg2apps.py
+++ msg2apps.py
@@ -4,6 +4,7 @@ import os
 
 
 def Wechat(msg, corpid, secret, agentid):
+    
     data = json.dumps({
             "touser" : "admin",
             "msgtype" : "text",
@@ -15,7 +16,7 @@ def Wechat(msg, corpid, secret, agentid):
             "enable_id_trans": 0,
             "enable_duplicate_check": 0,
             "duplicate_check_interval": 1800
-        })
+        })  
     url_get_token = "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={}&corpsecret={}".format(corpid, secret)
     try:
         token = requests.get(url_get_token, timeout=5).json()["access_token"]
@@ -23,10 +24,23 @@ def Wechat(msg, corpid, secret, agentid):
         print(e)
         return
     url = "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={}".format(token)
+    
     try:
-        # print(msg)
         r = requests.post(url, data=data, timeout=5)
         print(r.json()['errmsg'])
+        return
+    except Exception as e:
+        print(e)
+        return
+
+
+def Telegram(msg, token, chat_id):
+    
+    url = "https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}>".format(token, chat_id, msg)
+    try:
+        r = requests.post(url, timeout=5)
+        # TUDO
+        # print(r.json()['ok'])
         return
     except Exception as e:
         print(e)
